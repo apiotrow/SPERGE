db630f2078beae157e9f88924ea3f2efbf5f483c
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2016-02-04 14:13:38 -0800

    jukebox

---
+++ Assets/JukeBox.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * Controls JukeBox
+ */
+public class JukeBox : MonoBehaviour {
+	AudioSource audio;
+	AudioClip[] songs;
+	string[] songNames;
+
+	int song = 0;
+	string songName;
+
+	Button Btn_Next;
+	Button Btn_Prev;
+	Text Txt_Song;
+
+	void Start () {
+
+		//ui stuff
+		Btn_Next = transform.Find("Btn_Next").GetComponent<Button>();
+		Btn_Prev = transform.Find("Btn_Prev").GetComponent<Button>();
+		Txt_Song = transform.Find("BG_Song/Txt_Song").GetComponent<Text>();
+
+
+		//song stuff
+		audio = GetComponent<AudioSource>();
+
+		songs = System.Array.ConvertAll(Resources.LoadAll("Audio", typeof(AudioClip)),o=>(AudioClip)o);
+
+		songNames = new string[songs.Length];
+		for(int i = 0; i < songs.Length; i++){
+			songNames[i] = songs[i].name;
+		}
+
+		song = (int)Random.Range(0f, (float)songs.Length);
+		changeSong();
+
+
+		//button listeners
+		Btn_Next.onClick.AddListener(() => {
+			if(song < (songs.Length - 1))
+				song++;
+			else
+				song = 0;
+
+			changeSong();
+
+		});
+
+		Btn_Prev.onClick.AddListener(() => {
+			if(song > 0)
+				song--;
+			else
+				song = songs.Length - 1;
+			changeSong();
+
+		});
+	}
+
+	//reset song name and audioclip
+	public void changeSong(){
+		songName = songNames[song];
+		audio.clip = songs[song];
+		Txt_Song.text = songName;
+		audio.Play();
+	}
+}
---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public static class Utilities {
+
+	#region audio
+//	public static AudioClip[] loadSongs(string folderName){
+//		return System.Array.ConvertAll(Resources.LoadAll(folderName, typeof(AudioClip)),o=>(AudioClip)o);
+//	}
+//
+//	public static string[] loadSongNames(string folderName){
+//		AudioClip[] songs = System.Array.ConvertAll(Resources.LoadAll(folderName, typeof(AudioClip)),o=>(AudioClip)o);
+//
+//		string[] songNames = new string[songs.Length];
+//		for(int i = 0; i < songs.Length; i++){
+//			songNames[i] = songs[i].name;
+//		}
+//
+//		return songNames;
+//	}
+	#endregion audio
+}
